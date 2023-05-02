62a19a7ea130cbba5289584bd8def3e53d811e78
Rabby Khan <ajrabbyK72@gmail.com>
2023-05-02 13:14:20 +0600

    Update recipe image link

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const chefs = require("./data/chefs.json");
+const recipes = require("./data/recipes.json");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
