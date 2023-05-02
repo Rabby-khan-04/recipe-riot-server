ba9147ee0e276a20414a323e3cec974e0dcb9489
Rabby Khan <ajrabbyK72@gmail.com>
2023-05-02 09:39:33 +0600

    RecipeRiot project starts

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(
+    "Yippee! RecipeRiot's server is fired up and ready to go! Let's whip up some mouthwatering madness!"
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Hot diggity dog! Our server is up and running on port ${port}!`);
+});
